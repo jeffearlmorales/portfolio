34f50791b7d9b1e74df249ac5a62d9ec3c340e1c
Jeff Earl Morales <jeffearlmorales123@gmail.com>
2026-05-15 20:52:43 +0800

    Made Website Multi Page, Added Functions, Added Links, Organized Image, Added Paragraph

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ window.onscroll = () => {
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+
+const modal = document.getElementById("certificateModal");
+const modalImg = document.getElementById("enlargedImg");
+const cards = document.querySelectorAll(".project-card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", function (e) {
+    // If the user clicked the 'Review Certificate' button, don't open the popup
+    if (e.target.classList.contains("btn")) {
+      return;
+    }
+
+    // Otherwise, find the image inside THIS card and show it
+    const img = this.querySelector("img");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  });
+});
+
+// Close logic
+document.querySelector(".close").onclick = () => (modal.style.display = "none");
+
+window.onclick = (event) => {
+  if (event.target == modal) modal.style.display = "none";
+};
